31e7df4caf73d136f3c1ff9f62fe2779785ff2c8
DESKTOP-CLI22EN\HogenWang <hogenwang@vip.qq.com>
2018-04-19 23:56:02 +0800

    增加后台用户管理控制器

---
+++ COMCMS.Web/Areas/AdminCP/Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMCMS.Common;
+using COMCMS.Core;
+using XCode;
+using Newtonsoft.Json;
+using COMCMS.Web.Common;
+using COMCMS.Core.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace COMCMS.Web.Areas.AdminCP.Controllers
+{
+    public class MemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
