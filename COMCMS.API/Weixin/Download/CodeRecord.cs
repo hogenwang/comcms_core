acf28e8b6fe207ae21e8ab23d46cc038ae92cec4
DESKTOP-M4V1LH5\Jin <hogenwang@vip.qq.com>
2020-02-20 16:31:10 +0800

    完善微信公众号对接基础功能

---
+++ COMCMS.API/Weixin/Download/CodeRecord.cs
@@ -0,0 +1,18 @@
+﻿using Senparc.Weixin.MP.AdvancedAPIs.QrCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMCMS.API.Weixin.Download
+{
+    public class CodeRecord
+    {
+        public string Key { get; set; }
+        public int QrCodeId { get; set; }
+        public CreateQrCodeResult QrCodeTicket { get; set; }
+        public string Version { get; set; }
+        public bool Used { get; set; }
+        public bool AllowDownload { get; set; }
+        public bool IsWebVersion { get; set; }
+    }
+}
