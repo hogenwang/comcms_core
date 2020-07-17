3af9250443f7ee4aeb01390a41be2f157f7796a6
DESKTOP-M4V1LH5\Jin <hogenwang@vip.qq.com>
2020-07-17 18:08:54 +0800

    初步增加后台附件管理功能

--- COMCMS.Web/Areas/AdminCP/Controllers/AttachController.cs
+++ COMCMS.Web/Areas/AdminCP/Controllers/AttachController.cs
@@ -37,7 +37,13 @@ namespace COMCMS.Web.Areas.AdminCP.Controllers
         [MyAuthorize("viewlist", "attach")]
         public IActionResult AttachList()
         {
+            List<DirInfo> all = getAllAttach("");
+            string allJson = JsonConvert.SerializeObject(all);
+            //string basePath = $"{_env.WebRootPath}{Path.DirectorySeparatorChar}{attach.AttachPatch}{Path.DirectorySeparatorChar}";//基础目录
+
+            //string[] arrDirs = Directory.GetDirectories(basePath);
             return View();
+            //return Content(JsonConvert.SerializeObject(allJson));
         }
         #endregion
 
@@ -46,12 +52,49 @@ namespace COMCMS.Web.Areas.AdminCP.Controllers
         [MyAuthorize("viewlist", "attach","JSON")]
         public IActionResult GetDirList(string subdir)
         {
-            string attachPath = $"{_env.WebRootPath}{Path.DirectorySeparatorChar}{attach.AttachPatch}{Path.DirectorySeparatorChar}";
+            //string attachPath = $"{_env.WebRootPath}{Path.DirectorySeparatorChar}{attach.AttachPatch}{Path.DirectorySeparatorChar}";
 
             
 
             //Directory.GetDirectories()
             return Json(tip);
+        }
+        #endregion
+
+        #region 获取附件目录和所有子目录树
+        /// <summary>
+        /// 所有所有附件目录树
+        /// </summary>
+        /// <param name="subdir">子目录，需要完整，如：images/2020</param>
+        /// <returns></returns>
+        public List<DirInfo> getAllAttach(string subdir)
+        {
+            List<DirInfo> list = new List<DirInfo>();
+            string basePath = $"{_env.WebRootPath}{Path.DirectorySeparatorChar}{attach.AttachPatch}{Path.DirectorySeparatorChar}";//基础目录
+            if (!string.IsNullOrEmpty(subdir))
+            {
+                basePath += subdir.Replace("/", Path.DirectorySeparatorChar.ToString());
+            }
+
+            string[] arrDirs = Directory.GetDirectories(basePath);
+            
+            if (arrDirs !=null && arrDirs.Length > 0)
+            {
+                foreach (var item in arrDirs)
+                {
+                    string nextSubDir = subdir + "/" + item.Substring(item.LastIndexOf(Path.DirectorySeparatorChar)+1);
+                    var tpmList = getAllAttach(nextSubDir);
+                    list.Add(new DirInfo()
+                    {
+                        name = item.Substring(item.LastIndexOf(Path.DirectorySeparatorChar)+1),
+                        path = nextSubDir,
+                        //fullPath = item,
+                        subList = tpmList
+                    });
+                }
+            }
+
+            return list;
         }
         #endregion
     }
