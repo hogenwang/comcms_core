77b11016e33001096dfd5d21de6b28c886c839fc
DESKTOP-M4V1LH5\Jin <hogenwang@vip.qq.com>
2020-07-23 20:32:09 +0800

    增加部门管理

---
+++ COMCMS.Web/Areas/AdminCP/Controllers/DepartmentController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMCMS.Common;
+using COMCMS.Core;
+using XCode;
+using Newtonsoft.Json;
+using COMCMS.Web.Common;
+using Microsoft.AspNetCore.Http;
+
+namespace COMCMS.Web.Areas.AdminCP.Controllers
+{
+    public class DepartmentController : AdminBaseController
+    {
+        #region 部门管理
+        /// <summary>
+        /// 部门列表
+        /// </summary>
+        /// <returns></returns>
+        [MyAuthorize("viewlist", "department")]
+        public IActionResult DepartmentList()
+        {
+            IList<Department> list = Department.GetListTree(0, -1, false, true);
+            Core.Admin.WriteLogActions("查看部门列表页面；");
+            return View(list);
+        }
+
+
+        #endregion
+        #region 添加部门
+        [MyAuthorize("add", "department")]
+        public IActionResult AddDepartment()
+        {
+            //获取上级栏目
+            IList<Department> list = Department.GetListTree(0, 0, true, false);
+            ViewBag.ListTree = list;
+            Department entity = new Department();
+            Core.Admin.WriteLogActions("查看添加部门页面；");
+            return View(entity);
+        }
+
+        [HttpPost]
+        [MyAuthorize("add", "department", "JSON")]
+        public IActionResult AddDepartment(Department model)
+        {
+            //获取上级栏目
+            if (string.IsNullOrWhiteSpace(model.DepartmentName))
+            {
+                tip.Message = "部门名称不能为空！";
+                return Json(tip);
+            }
+
+            model.Insert();
+            Core.Admin.WriteLogActions("添加部门(id:" + model.Id + ");");
+            tip.Status = JsonTip.SUCCESS;
+            tip.Message = "添加部门成功";
+            tip.ReturnUrl = "close";
+
+            return Json(tip);
+        }
+        #endregion
+
+        #region 编辑部门
+        //编辑部门
+        [HttpGet]
+        [MyAuthorize("edit", "department")]
+        public IActionResult EditDepartment(int id)
+        {
+            Department entity = Department.Find(Department._.Id == id);
+            if (entity == null)
+            {
+                return EchoTipPage("系统找不到本记录！");
+            }
+            //获取上级栏目
+            IList<Department> list = Department.GetListTree(0, 0, true, false);
+            ViewBag.ListTree = list;
+            Core.Admin.WriteLogActions("查看/编辑部门（id:" + id + "）页面；");
+            return View(entity);
+        }
+
+        [HttpPost]
+        [MyAuthorize("edit", "department", "JSON")]
+        public IActionResult EditDepartment(Department model)
+        {
+            if (model.Id <= 0)
+            {
+                tip.Message = "错误参数传递！";
+                Json(tip);
+            }
+            if (string.IsNullOrWhiteSpace(model.DepartmentName))
+            {
+                tip.Message = "部门名称不能为空！";
+                return Json(tip);
+            }
+            Department entity = Department.Find(Department._.Id == model.Id);
+            if (entity == null)
+            {
+                tip.Message = "系统找不到本记录！";
+                Json(tip);
+            }
+            //赋值
+            
+            entity.DepartmentName = model.DepartmentName;
+            entity.Pic = model.Pic;
+            entity.DepartmentDescription = model.DepartmentDescription;
+            entity.Rank = model.Rank;
+            entity.Pic = model.Pic;
+            entity.Tel = model.Tel;
+            entity.Fax = model.Fax;
+            entity.Email = model.Email;
+            entity.IsNotAllowDel = Request.Form["IsNotAllowDel"] == "1" ? 1 : 0;
+            //如果修改了路径
+            if (entity.PId != model.PId)
+            {
+                entity.PId = model.PId;
+                entity.Location = Department.GetNewLocation(model.PId);
+            }
+            entity.Save();
+
+            Core.Admin.WriteLogActions("修改部门(id:" + entity.Id + ");");
+            tip.Status = JsonTip.SUCCESS;
+            tip.Message = "修改部门成功";
+            tip.ReturnUrl = "close";
+
+            return Json(tip);
+        }
+
+        //删除栏目
+        [HttpPost]
+        [MyAuthorize("del", "department", "JSON")]
+        public JsonResult DelDepartment(int id)
+        {
+            Department entity = Department.Find(Department._.Id == id);
+            if (entity == null)
+            {
+                tip.Message = "系统找不到本部门！";
+                return Json(tip);
+            }
+            if (Department.FindCount(Department._.PId == entity.Id, null, null, 0, 0) > 0)
+            {
+                tip.Message = "部门目有下级部门，不允许删除！";
+                return Json(tip);
+            }
+            //删除文章
+
+            Core.Admin.WriteLogActions("删除部门(id:" + entity.Id + ");");
+            entity.IsDel =1;
+            entity.Update();
+            tip.Status = JsonTip.SUCCESS;
+            tip.Message = "删除部门成功";
+            return Json(tip);
+        }
+        #endregion
+    }
+}
---
+++ XCoder/Entity/数据字典.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using XCode;
+using XCode.Configuration;
+using XCode.DataAccessLayer;
+
+namespace COMCMS.Core
+{
+    /// <summary>数据字典</summary>
+    [Serializable]
+    [DataObject]
+    [Description("数据字典")]
+    [BindTable("DataDictionary", Description = "数据字典", ConnName = "dbconn", DbType = DatabaseType.SqlServer)]
+    public partial class DataDictionary : IDataDictionary
+    {
+        #region 属性
+        private Int32 _Id;
+        /// <summary>编号</summary>
+        [DisplayName("编号")]
+        [Description("编号")]
+        [DataObjectField(true, true, false, 0)]
+        [BindColumn("Id", "编号", "")]
+        public Int32 Id { get => _Id; set { if (OnPropertyChanging(__.Id, value)) { _Id = value; OnPropertyChanged(__.Id); } } }
+
+        private String _Key;
+        /// <summary>字典key</summary>
+        [DisplayName("字典key")]
+        [Description("字典key")]
+        [DataObjectField(false, false, true, 50)]
+        [BindColumn("Key", "字典key", "")]
+        public String Key { get => _Key; set { if (OnPropertyChanging(__.Key, value)) { _Key = value; OnPropertyChanged(__.Key); } } }
+
+        private String _Description;
+        /// <summary>字典介绍</summary>
+        [DisplayName("字典介绍")]
+        [Description("字典介绍")]
+        [DataObjectField(false, false, true, 50)]
+        [BindColumn("Description", "字典介绍", "")]
+        public String Description { get => _Description; set { if (OnPropertyChanging(__.Description, value)) { _Description = value; OnPropertyChanged(__.Description); } } }
+
+        private Int32 _Rank;
+        /// <summary>排序</summary>
+        [DisplayName("排序")]
+        [Description("排序")]
+        [DataObjectField(false, false, false, 0)]
+        [BindColumn("Rank", "排序", "")]
+        public Int32 Rank { get => _Rank; set { if (OnPropertyChanging(__.Rank, value)) { _Rank = value; OnPropertyChanged(__.Rank); } } }
+
+        private DateTime _AddTime;
+        /// <summary>添加时间</summary>
+        [DisplayName("添加时间")]
+        [Description("添加时间")]
+        [DataObjectField(false, false, true, 0)]
+        [BindColumn("AddTime", "添加时间", "")]
+        public DateTime AddTime { get => _AddTime; set { if (OnPropertyChanging(__.AddTime, value)) { _AddTime = value; OnPropertyChanged(__.AddTime); } } }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>获取/设置 字段值</summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        public override Object this[String name]
+        {
+            get
+            {
+                switch (name)
+                {
+                    case __.Id: return _Id;
+                    case __.Key: return _Key;
+                    case __.Description: return _Description;
+                    case __.Rank: return _Rank;
+                    case __.AddTime: return _AddTime;
+                    default: return base[name];
+                }
+            }
+            set
+            {
+                switch (name)
+                {
+                    case __.Id: _Id = value.ToInt(); break;
+                    case __.Key: _Key = Convert.ToString(value); break;
+                    case __.Description: _Description = Convert.ToString(value); break;
+                    case __.Rank: _Rank = value.ToInt(); break;
+                    case __.AddTime: _AddTime = value.ToDateTime(); break;
+                    default: base[name] = value; break;
+                }
+            }
+        }
+        #endregion
+
+        #region 字段名
+        /// <summary>取得数据字典字段信息的快捷方式</summary>
+        public partial class _
+        {
+            /// <summary>编号</summary>
+            public static readonly Field Id = FindByName(__.Id);
+
+            /// <summary>字典key</summary>
+            public static readonly Field Key = FindByName(__.Key);
+
+            /// <summary>字典介绍</summary>
+            public static readonly Field Description = FindByName(__.Description);
+
+            /// <summary>排序</summary>
+            public static readonly Field Rank = FindByName(__.Rank);
+
+            /// <summary>添加时间</summary>
+            public static readonly Field AddTime = FindByName(__.AddTime);
+
+            static Field FindByName(String name) => Meta.Table.FindByName(name);
+        }
+
+        /// <summary>取得数据字典字段名称的快捷方式</summary>
+        public partial class __
+        {
+            /// <summary>编号</summary>
+            public const String Id = "Id";
+
+            /// <summary>字典key</summary>
+            public const String Key = "Key";
+
+            /// <summary>字典介绍</summary>
+            public const String Description = "Description";
+
+            /// <summary>排序</summary>
+            public const String Rank = "Rank";
+
+            /// <summary>添加时间</summary>
+            public const String AddTime = "AddTime";
+        }
+        #endregion
+    }
+
+    /// <summary>数据字典接口</summary>
+    public partial interface IDataDictionary
+    {
+        #region 属性
+        /// <summary>编号</summary>
+        Int32 Id { get; set; }
+
+        /// <summary>字典key</summary>
+        String Key { get; set; }
+
+        /// <summary>字典介绍</summary>
+        String Description { get; set; }
+
+        /// <summary>排序</summary>
+        Int32 Rank { get; set; }
+
+        /// <summary>添加时间</summary>
+        DateTime AddTime { get; set; }
+        #endregion
+
+        #region 获取/设置 字段值
+        /// <summary>获取/设置 字段值</summary>
+        /// <param name="name">字段名</param>
+        /// <returns></returns>
+        Object this[String name] { get; set; }
+        #endregion
+    }
+}
